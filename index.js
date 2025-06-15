9cbbe77dac88fe3ccdaca9019a2bd291e4977df6
DANK1775 <fabianepico@gmail.com>
2025-06-15 12:51:26 -0600

    update file

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-<<<<<<< HEAD
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -14,13 +12,6 @@ try {
 } catch (error) {
     console.log(error)
 }
-=======
-const {router} = require('./routes')
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(router)
->>>>>>> 6016629a71b8ada54ed6f07ff91dfaa020e3a6f0
 
 app.listen(port, () => console.log(`app listen ${port}`))
 
